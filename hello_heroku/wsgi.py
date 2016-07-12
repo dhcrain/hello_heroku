7bb25cf9f7fed610360b8641be19b65eff32230c
dhcrain <dhcrain@gmail.com>
2016-07-12 17:51:42 -0400

    moved lines around in the wsgi file

--- hello_heroku/wsgi.py
+++ hello_heroku/wsgi.py
@@ -12,8 +12,8 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
-
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hello_heroku.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
