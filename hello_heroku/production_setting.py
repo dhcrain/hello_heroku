6d2a1aa83d00eb63147683c0923e8bd2dfa6aba0
dhcrain <dhcrain@gmail.com>
2016-07-18 11:35:25 -0400

    made changes in calls with Joel

---
+++ hello_heroku/production_setting.py
@@ -0,0 +1,5 @@
+from hello_heroku.settings import *
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
--- hello_heroku/settings.py
+++ hello_heroku/settings.py
@@ -1,5 +1,6 @@
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -127,3 +128,8 @@ STATICFILES_DIRS = (
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+import dj_database_url
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
